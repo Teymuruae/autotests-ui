1af171f8652fb22cd85f8c9a785f1ee9f609aae8
teymuruae <baratov.teymur@yandex.ru>
2026-05-13 15:55:20 +0300

    pytest playwright

---
+++ tests/test_authorization.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+def test_wrong_email_or_password_authorization():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+
+        email_field = page.locator('//div[@data-testid="login-form-email-input"]//input')
+        password_field = page.get_by_test_id('login-form-password-input').locator('input')
+        login_button = page.locator('//button[@id = "login-page-login-button"]')
+        alert = page.locator("//div[contains(@class, 'MuiAlert-message')]")
+
+        email_field.fill("rugaga@email.ru")
+        password_field.fill("password")
+        login_button.click()
+
+        expect(alert).to_be_visible()
+        expect(alert).to_have_text('Wrong email or password')
