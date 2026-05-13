ad10d7a5604b52b92ba33bd1834ca250bb204c3c
teymuruae <baratov.teymur@yandex.ru>
2026-05-13 15:55:36 +0300

    Интеграция Pytest и Playwright. Практика написания автотестов

---
+++ tests/test_courses.py
@@ -0,0 +1,42 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+def test_empty_courses_list():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        email_input = page.get_by_test_id("registration-form-email-input").locator('input')
+        username_input = page.get_by_test_id("registration-form-username-input").locator('input')
+        password_input = page.get_by_test_id("registration-form-password-input").locator('input')
+        registration_button = page.get_by_test_id("registration-page-registration-button")
+
+        email_input.fill("user.name@gmail.com")
+        username_input.fill("username")
+        password_input.fill("password")
+        registration_button.click()
+
+        context.storage_state(path="browser-state.json")
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state="browser-state.json")
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+        title = page.get_by_test_id("courses-list-toolbar-title-text")
+        no_results_text = page.get_by_test_id("courses-list-empty-view-title-text")
+        no_results_icon = page.get_by_test_id("courses-list-empty-view-icon")
+        no_results_descr = page.get_by_test_id("courses-list-empty-view-description-text")
+
+        expect(title).to_be_visible()
+        expect(title).to_have_text("Courses")
+        expect(no_results_text).to_be_visible()
+        expect(no_results_text).to_have_text("There is no results")
+        expect(no_results_icon).to_be_visible()
+        expect(no_results_descr).to_be_visible()
+        expect(no_results_descr).to_have_text("Results from the load test pipeline will be displayed here")
