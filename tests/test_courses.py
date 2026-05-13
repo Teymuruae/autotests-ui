2a36907e97dbb6284a858b5476e9cd6dfe71aaf2
teymuruae <baratov.teymur@yandex.ru>
2026-05-13 22:01:27 +0300

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,44 +1,21 @@
 from playwright.sync_api import sync_playwright, expect
 import pytest
 
+
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+def test_empty_courses_list(chromium_page_with_state):
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    title = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
+    no_results_text = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
+    no_results_icon = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
+    no_results_descr = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
 
-        email_input = page.get_by_test_id("registration-form-email-input").locator('input')
-        username_input = page.get_by_test_id("registration-form-username-input").locator('input')
-        password_input = page.get_by_test_id("registration-form-password-input").locator('input')
-        registration_button = page.get_by_test_id("registration-page-registration-button")
-
-        email_input.fill("user.name@gmail.com")
-        username_input.fill("username")
-        password_input.fill("password")
-        registration_button.click()
-
-        context.storage_state(path="browser-state.json")
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json")
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-
-        title = page.get_by_test_id("courses-list-toolbar-title-text")
-        no_results_text = page.get_by_test_id("courses-list-empty-view-title-text")
-        no_results_icon = page.get_by_test_id("courses-list-empty-view-icon")
-        no_results_descr = page.get_by_test_id("courses-list-empty-view-description-text")
-
-        expect(title).to_be_visible()
-        expect(title).to_have_text("Courses")
-        expect(no_results_text).to_be_visible()
-        expect(no_results_text).to_have_text("There is no results")
-        expect(no_results_icon).to_be_visible()
-        expect(no_results_descr).to_be_visible()
-        expect(no_results_descr).to_have_text("Results from the load test pipeline will be displayed here")
+    expect(title).to_be_visible()
+    expect(title).to_have_text("Courses")
+    expect(no_results_text).to_be_visible()
+    expect(no_results_text).to_have_text("There is no results")
+    expect(no_results_icon).to_be_visible()
+    expect(no_results_descr).to_be_visible()
+    expect(no_results_descr).to_have_text("Results from the load test pipeline will be displayed here")
