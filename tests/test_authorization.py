a02b969cb04a2d865952b0491bf0fb8a22346ffa
teymuruae <baratov.teymur@yandex.ru>
2026-05-14 10:28:17 +0300

    Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -4,7 +4,12 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page):
+@pytest.mark.parametrize("email, password", [
+    ("user.name@gmail.com", "password"),
+    ("user.name@gmail.com", "  "),
+    ("  ", "password"),
+])
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
 
     email_field = chromium_page.locator('//div[@data-testid="login-form-email-input"]//input')
@@ -12,8 +17,8 @@ def test_wrong_email_or_password_authorization(chromium_page: Page):
     login_button = chromium_page.locator('//button[@id = "login-page-login-button"]')
     alert = chromium_page.locator("//div[contains(@class, 'MuiAlert-message')]")
 
-    email_field.fill("rugaga@email.ru")
-    password_field.fill("password")
+    email_field.fill(email)
+    password_field.fill(password)
     login_button.click()
 
     expect(alert).to_be_visible()
