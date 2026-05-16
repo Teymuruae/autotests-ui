44d71e545743025ce917136eaffe31b44c0f80c9
teymuruae <baratov.teymur@yandex.ru>
2026-05-16 18:36:22 +0300

    Добавление Allure шагов в автотесты. Практика работы с allure.step

--- components/courses/create_course_exercises_toolbar_view_component.py
+++ components/courses/create_course_exercises_toolbar_view_component.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
@@ -13,6 +14,7 @@ class CreateCourseExercisesToolbarViewComponent(BaseComponent):
         self.create_exercise_button = Button(page, 'create-course-exercises-box-toolbar-create-exercise-button',
                                              'Create execrice')
 
+    @allure.step("Check visible exercises toolbar")
     def check_visible(self):
         self.exercises_title.check_visible()
         self.exercises_title.check_have_text('Exercises')
